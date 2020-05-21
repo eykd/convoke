823bb208d8e5c399f6e22d60affd56eecd0433dd
deyk <deyk@crossway.org>
2020-05-20 20:31:56 -0700

    Improve test invocation

--- runtests.sh
+++ runtests.sh
@@ -1,1 +1,11 @@
-pytest --flake8 --cov=src --cov-branch $@
+#!/bin/sh
+set -e
+set -x
+pytest \
+    --failed-first \
+    --new-first \
+    --exitfirst \
+    --flake8 \
+    --cov=src \
+    --cov-branch \
+    --no-cov-on-fail $@
